bcb2f56fad26cdedd11f4a7a017e53c45ef96605
MushaBoy <developermusha@gmail.com>
2024-10-01 17:24:05 +0530

    Create all Website Files

---
+++ js/about-page.js
@@ -0,0 +1,39 @@
+
+  // automatic typing section start
+
+var typed = new Typed(".auto-type-1", {
+    strings: ["I am a front-end web developer. with 5+ years of experience specializing in HTML5, JavaScript, PHP, MySQL, database design, and front-end development. I can provide clean code and pixel perfect design. I also make the website more and more interactive with web animations. A responsive design makes your website accessible to all users, regardless of their device."],
+    typeSpeed: 110,
+    backSpeed: 110,
+    loop: false,
+  });
+  
+  // automatic typing section end
+  
+
+
+  
+  // automatic typing section start
+
+var typed = new Typed(".auto-type-2", {
+  strings: ["I am a graphic designer. with 5+ years of experience specializing in Photo Shop, Illustrator, Corel Draw, Gimp, logo design, and all graphic design. I can provide attractive design and pixel perfect design. I also make the design more and more interactive with victor-based design. A responsive design makes your marketing attractive to all users, regardless of their device. "],
+  typeSpeed: 110,
+  backSpeed: 110,
+  loop: false,
+});
+
+// automatic typing section end
+
+
+
+  // automatic typing section start
+
+  var typed = new Typed(".auto-type-3", {
+    strings: ["I am a computer hardware technician. with 5+ years of experience specializing in Windows, Mac, Linux, platform. trouble shooting, maintenance, all kinds of desktop, laptop. I can provide professional service and door to door service. I also make the computer running like a new with my experienced service. A good computer makes your work easy to do. Depending on your computer."],
+    typeSpeed: 110,
+    backSpeed: 110,
+    loop: false,
+  });
+  
+  // automatic typing section end
+  
---
+++ js/script.js
@@ -0,0 +1,10 @@
+// automatic typing section start
+
+var typed = new Typed(".auto-type", {
+  strings: ["Developer", "Freelancer", "Designer"],
+  typeSpeed: 150,
+  backSpeed: 150,
+  loop: true,
+});
+
+// automatic typing section end
